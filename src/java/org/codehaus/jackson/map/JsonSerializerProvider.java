abdd1ce94cdc8a93b230bfc78103c6677b3fabca
Tatu Saloranta <tatu@fasterxml.com>
2008-12-08 06:17:21 +0000

    Minor work on data binding.

--- src/java/org/codehaus/jackson/map/JsonSerializerProvider.java
+++ src/java/org/codehaus/jackson/map/JsonSerializerProvider.java
@@ -7,6 +7,12 @@ package org.codehaus.jackson.map;
  */
 public abstract class JsonSerializerProvider
 {
+    /*
+    //////////////////////////////////////////////////////
+    // General serializer locating methods
+    //////////////////////////////////////////////////////
+     */
+
     /**
      * Method called to get hold of a serializer for given type; or
      * if no such serializer can be found, a default handler (which
@@ -14,4 +20,28 @@ public abstract class JsonSerializerProvider
      * throw an exception when invoked).
      */
     public abstract <T> JsonSerializer<T> findSerializer(Class<T> type);
+
+    /*
+    //////////////////////////////////////////////////////
+    // Accessors for specialized serializers
+    //////////////////////////////////////////////////////
+     */
+
+    /**
+     * Method called to get the serializer to use for serializing
+     * Map keys that are nulls: this is needed since Json does not allow
+     * any non-String value as key, including null.
+     * Typically, resulting serializer
+     * will either throw an exception, or use an empty String; but
+     * other behaviors are possible.
+     */
+    public abstract SimpleJsonSerializer getNullKeySerializer();
+
+    /**
+     * Method called to get the serializer to use for serializing
+     * values (root level, Array members or List field values)
+     * that are nulls. Specific accessor is needed because nulls
+     * in Java do not contain type information.
+     */
+    public abstract SimpleJsonSerializer getNullValueSerializer();
 }
