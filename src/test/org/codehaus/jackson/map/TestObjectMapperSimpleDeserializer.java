83d750e9f2ad3afef4dd0e367578b37da31a88a4
Tatu Saloranta <tatu@fasterxml.com>
2009-01-09 01:01:55 +0000

    ...

--- src/test/org/codehaus/jackson/map/TestObjectMapperSimpleDeserializer.java
+++ src/test/org/codehaus/jackson/map/TestObjectMapperSimpleDeserializer.java
@@ -171,61 +171,6 @@ public class TestObjectMapperSimpleDeserializer
         assertEquals(value, new ObjectMapper().readValue("\""+value.toString()+"\"", URI.class));
     }
 
-    public void testDateUtil() throws Exception
-    {
-        // not ideal, to use (ever-changing) current date, but...
-        java.util.Date value = new java.util.Date();
-        long now = value.getTime();
-
-        // First from long
-        assertEquals(value, new ObjectMapper().readValue(""+now, java.util.Date.class));
-
-        String dateStr = serializeDateAsString(value);
-        java.util.Date result = new ObjectMapper().readValue("\""+dateStr+"\"", java.util.Date.class);
-
-        assertEquals("Date: expect "+value+" ("+value.getTime()+"), got "+result+" ("+result.getTime()+")", value.getTime(), result.getTime());
-    }
-
-    public void testDateSql() throws Exception
-    {
-        // not ideal, to use (ever-changing) current date, but...
-        long now = System.currentTimeMillis();
-        java.sql.Date value = new java.sql.Date(now);
-
-        // First from long
-        assertEquals(value, new ObjectMapper().readValue(""+now, java.sql.Date.class));
-        // then from String
-        
-        String expStr = value.toString();
-        java.sql.Date result = new ObjectMapper().readValue("\""+expStr+"\"", java.sql.Date.class);
-        String actStr = result.toString();
-
-        /* 07-Jan-2009, tatu: Ok; things get weird here: java.sql.Date
-         *   does NOT override equals() method. But that's just plain wrong,
-         *   as it should NOT compare time part, as it's not supposed to even
-         *   exist (essentially) for this type. So, let's compare String
-         *   representation, not timestamp
-         */
-
-        assertEquals(actStr, expStr);
-    }
-
-    public void testCalendar() throws Exception
-    {
-        // not ideal, to use (ever-changing) current date, but...
-        java.util.Date now = new Date();
-        java.util.Calendar value = Calendar.getInstance();
-        value.setTime(now);
-
-        // First from long
-        assertEquals(value, new ObjectMapper().readValue(""+now.getTime(), Calendar.class));
-
-        String dateStr = serializeDateAsString(now);
-        Calendar result = new ObjectMapper().readValue("\""+dateStr+"\"", Calendar.class);
-
-        assertEquals(value, result);
-    }
-
     public void testEnum() throws Exception
     {
         /* First, "good" case with Strings
@@ -302,21 +247,6 @@ public class TestObjectMapperSimpleDeserializer
             Integer result = mapper.readValue(jp, Integer.class);
             assertEquals(Integer.valueOf(i), result);
         }
-    }
-
-    /*
-    //////////////////////////////////////////////////////////
-    // Helper methods
-    //////////////////////////////////////////////////////////
-     */
-
-    String serializeDateAsString(java.util.Date value)
-    {
-        /* Then from String. This is bit tricky, since JDK does not really
-         * suggest a 'standard' format. So let's try using something...
-         */
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-        return df.format(value);
     }
 }
 
