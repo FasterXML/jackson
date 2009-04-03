8728e094c6b191ab12563599a2e719e280db0efe
Tatu Saloranta <tatu@fasterxml.com>
2009-04-03 21:37:07 +0000

    Found a problem wrt deserializing simple exceptions.

---
+++ src/test/org/codehaus/jackson/map/deser/TestExceptionDeserialization.java
@@ -0,0 +1,28 @@
+package org.codehaus.jackson.map.deser;
+
+import org.codehaus.jackson.map.BaseMapTest;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.codehaus.jackson.map.*;
+
+/**
+ * Unit tests for verifying that simple exceptions can be deserialized.
+ */
+public class TestExceptionDeserialization
+    extends BaseMapTest
+{
+    public void testIOException()
+        throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        IOException ioe = new IOException("TEST");
+
+        String json = serializeAsString(mapper, ioe);
+
+        IOException result = mapper.readValue(json, IOException.class);
+
+        assertEquals(ioe.getMessage(), result.getMessage());
+    }
+}
