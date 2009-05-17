1383a1986c3aa3763a91743ce60e73f2d829024c
Tatu Saloranta <tatu@fasterxml.com>
2009-05-17 22:00:47 +0000

    Adding a unit test to track a minor regression that occured in trunk

---
+++ src/test/org/codehaus/jackson/map/ser/TestAnnotationIgnore.java
@@ -0,0 +1,108 @@
+package org.codehaus.jackson.map.ser;
+
+import org.codehaus.jackson.map.BaseMapTest;
+
+import java.io.*;
+import java.util.*;
+
+import org.codehaus.jackson.*;
+import org.codehaus.jackson.annotate.*;
+import org.codehaus.jackson.map.*;
+
+/**
+ * This unit test suite tests use of Annotations for
+ * bean serialization.
+ */
+public class TestAnnotationIgnore
+    extends BaseMapTest
+{
+    /*
+    //////////////////////////////////////////////
+    // Annotated helper classes
+    //////////////////////////////////////////////
+     */
+
+    /// Class for testing enabled {@link JsonIgnore} annotation
+    final static class SizeClassEnabledIgnore
+    {
+        // note: must be public to be seen
+        public int getX() { return 1; }
+        @JsonIgnore public int getY() { return 9; }
+    }
+
+    /// Class for testing disabled {@link JsonIgnore} annotation
+    final static class SizeClassDisabledIgnore
+    {
+        // note: must be public to be seen
+        public int getX() { return 3; }
+        @JsonIgnore(false) public int getY() { return 4; }
+    }
+
+    static class BaseClassIgnore
+    {
+        @JsonGetter("x")
+        @JsonIgnore
+        public int x() { return 1; }
+
+        public int getY() { return 2; }
+    }
+
+    static class SubClassNonIgnore
+        extends BaseClassIgnore
+    {
+        /* Annotations to disable ignorance, in sub-class; note that
+         * we must still get "JsonGetter" fro super class
+         */
+        @Override
+        @JsonIgnore(false)
+        public int x() { return 3; }
+    }
+
+    /*
+    /*
+    //////////////////////////////////////////////
+    // Main tests
+    //////////////////////////////////////////////
+     */
+
+    public void testSimpleIgnore() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+        // Should see "x", not "y"
+        Map<String,Object> result = writeAndMap(m, new SizeClassEnabledIgnore());
+        assertEquals(1, result.size());
+        assertEquals(Integer.valueOf(1), result.get("x"));
+        assertNull(result.get("y"));
+    }
+
+    public void testDisabledIgnore() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+        // Should see "x" and "y"
+        Map<String,Object> result = writeAndMap(m, new SizeClassDisabledIgnore());
+        assertEquals(2, result.size());
+        assertEquals(Integer.valueOf(3), result.get("x"));
+        assertEquals(Integer.valueOf(4), result.get("y"));
+    }
+
+    /**
+     * Test case to verify that ignore tag can also be disabled
+     * via inheritance
+     */
+    public void testIgnoreOver() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+
+        // should only see "y"
+        Map<String,Object> result = writeAndMap(m, new BaseClassIgnore());
+        assertEquals(1, result.size());
+        assertEquals(Integer.valueOf(2), result.get("y"));
+
+        // Should see "x" and "y"
+        result = writeAndMap(m, new SubClassNonIgnore());
+        assertEquals(2, result.size());
+        assertEquals(Integer.valueOf(3), result.get("x"));
+        assertEquals(Integer.valueOf(2), result.get("y"));
+    }
+
+}
