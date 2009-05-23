77268079e4e570884199b2e4dea366b00ab1fc4f
Tatu Saloranta <tatu@fasterxml.com>
2009-05-23 01:55:23 +0000

    Fixed [JACKSON-121], problems deserializing some ISO-8601 serialized Dates.

--- src/java/org/codehaus/jackson/map/util/StdDateFormat.java
+++ src/java/org/codehaus/jackson/map/util/StdDateFormat.java
@@ -18,10 +18,17 @@ public class StdDateFormat
     extends DateFormat
 {
     /**
-     * This constant defines a commonly used date format that conforms
-     * to ISO-8601 date formatting standard.
+     * Defines a commonly used date format that conforms
+     * to ISO-8601 date formatting standard, when it includes basic undecorated
+     * timezone definition
      */
     final static String DATE_FORMAT_STR_ISO8601 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+
+    /**
+     * Same as 'regular' 8601, but handles 'Z' as an alias for "+0000"
+     * (or "GMT")
+     */
+    final static String DATE_FORMAT_STR_ISO8601_Z = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     /**
      * This constant defines the date format specified by
@@ -29,8 +36,19 @@ public class StdDateFormat
      */
     final static String DATE_FORMAT_STR_RFC1123 = "EEE, dd MMM yyyy HH:mm:ss zzz";
 
-    final static SimpleDateFormat DATE_FORMAT_ISO8601;
+    /**
+     * For error messages we'll also need a list of all formats.
+     */
+    final static String[] ALL_FORMATS = new String[] {
+        DATE_FORMAT_STR_ISO8601,
+        DATE_FORMAT_STR_ISO8601_Z,
+        DATE_FORMAT_STR_RFC1123
+    };
+
     final static SimpleDateFormat DATE_FORMAT_RFC1123;
+
+    final static SimpleDateFormat DATE_FORMAT_ISO8601;
+    final static SimpleDateFormat DATE_FORMAT_ISO8601_Z;
 
     /* Let's construct "blueprint" date format instances: can not be used
      * as is, due to thread-safety issues, but can be used for constructing
@@ -41,10 +59,12 @@ public class StdDateFormat
          * baseline DataFormat objects
          */
         TimeZone gmt = TimeZone.getTimeZone("GMT");
-        DATE_FORMAT_ISO8601 = new SimpleDateFormat(DATE_FORMAT_STR_ISO8601);
-        DATE_FORMAT_ISO8601.setTimeZone(gmt);
         DATE_FORMAT_RFC1123 = new SimpleDateFormat(DATE_FORMAT_STR_RFC1123);
         DATE_FORMAT_RFC1123.setTimeZone(gmt);
+        DATE_FORMAT_ISO8601 = new SimpleDateFormat(DATE_FORMAT_STR_ISO8601);
+        DATE_FORMAT_ISO8601.setTimeZone(gmt);
+        DATE_FORMAT_ISO8601_Z = new SimpleDateFormat(DATE_FORMAT_STR_ISO8601_Z);
+        DATE_FORMAT_ISO8601_Z.setTimeZone(gmt);
     }
 
     /**
@@ -52,8 +72,9 @@ public class StdDateFormat
      */
     public final static StdDateFormat instance = new StdDateFormat();
 
-    transient DateFormat _formatISO8601;
-    transient DateFormat _formatRFC1123;
+    transient SimpleDateFormat _formatRFC1123;
+    transient SimpleDateFormat _formatISO8601;
+    transient SimpleDateFormat _formatISO8601_z;
 
     /*
     /////////////////////////////////////////////////////
@@ -86,7 +107,7 @@ public class StdDateFormat
      * compliant date format.
      */
     public static DateFormat getISO8601Format(TimeZone tz) {
-        DateFormat df = (DateFormat) DATE_FORMAT_ISO8601.clone();
+        DateFormat df = (SimpleDateFormat) DATE_FORMAT_ISO8601.clone();
         df.setTimeZone(tz);
         return df;
     }
@@ -108,7 +129,7 @@ public class StdDateFormat
      */
     public static DateFormat getRFC1123Format(TimeZone tz)
     {
-        DateFormat df = (DateFormat) DATE_FORMAT_RFC1123.clone();
+        DateFormat df = (SimpleDateFormat) DATE_FORMAT_RFC1123.clone();
         df.setTimeZone(tz);
         return df;
     }
@@ -125,35 +146,46 @@ public class StdDateFormat
         dateStr = dateStr.trim();
         ParsePosition pos = new ParsePosition(0);
         Date result = parse(dateStr, pos);
-        if (result == null) {
-            throw new ParseException
-                (String.format("Can not parse date \"%s\": not compatible with any of standard forms (\"%s\" or \"%s\")",
-                               dateStr,
-                               DATE_FORMAT_STR_ISO8601,
-                               DATE_FORMAT_STR_RFC1123
-                               ), pos.getErrorIndex());
+        if (result != null) {
+            return result;
         }
-        return result;
+
+        StringBuilder sb = new StringBuilder();
+        for (String f : ALL_FORMATS) {
+            if (sb.length() > 0) {
+                sb.append("\", \"");
+            } else {
+                sb.append('"');
+            }
+            sb.append(f);
+        }
+        sb.append('"');
+        throw new ParseException
+            (String.format("Can not parse date \"%s\": not compatible with any of standard forms (%s)",
+                           dateStr, sb.toString()), pos.getErrorIndex());
     }
 
     public Date parse(String dateStr, ParsePosition pos)
     {
-
-        return findLikeliestFormat(dateStr).parse(dateStr, pos);
+        if (looksLikeISO8601(dateStr)) {
+            return parseAsISO8601(dateStr, pos);
+        }
+        // Otherwise, fall back to using RFC 1123
+        return parseAsRFC1123(dateStr, pos);
     }
 
     public StringBuffer format(Date date, StringBuffer toAppendTo,
                                FieldPosition fieldPosition)
     {
         if (_formatISO8601 == null) {
-            _formatISO8601 = (DateFormat) DATE_FORMAT_ISO8601.clone();
+            _formatISO8601 = (SimpleDateFormat) DATE_FORMAT_ISO8601.clone();
         }
         return _formatISO8601.format(date, toAppendTo, fieldPosition);
     }
 
     /*
     /////////////////////////////////////////////////////
-    // Helper method
+    // Helper methods
     /////////////////////////////////////////////////////
      */
 
@@ -161,23 +193,64 @@ public class StdDateFormat
      * Overridable helper method used to figure out which of supported
      * formats is the likeliest match.
      */
-    protected DateFormat findLikeliestFormat(String dateStr)
+    protected boolean looksLikeISO8601(String dateStr)
     {
         if (dateStr.length() >= 5
             && Character.isDigit(dateStr.charAt(0))
             && Character.isDigit(dateStr.charAt(3))
             && dateStr.charAt(4) == '-'
             ) {
-            if (_formatISO8601 == null) {
-                _formatISO8601 = (DateFormat) DATE_FORMAT_ISO8601.clone();
+            return true;
+        }
+        return false;
+    }
+
+    protected Date parseAsISO8601(String dateStr, ParsePosition pos)
+    {
+        /* 21-May-2009, tatu: SimpleDateFormat has very strict handling of
+         * timezone  modifiers for ISO-8601. So we need to do some scrubbing.
+         */
+
+        /* First: do we have "zulu" format ('Z' == "GMT")? If yes, that's
+         * quite simple because we already set date format timezone to be
+         * GMT, and hence can just strip out 'Z' altogether
+         */
+        int len = dateStr.length();
+        char c = dateStr.charAt(len-1);
+        SimpleDateFormat df;
+
+        if (c == 'Z') {
+            df = _formatISO8601_z;
+            if (df == null) {
+                df = _formatISO8601_z = (SimpleDateFormat) DATE_FORMAT_ISO8601_Z.clone();
             }
-            return _formatISO8601;
+        } else {
+            c = dateStr.charAt(len-3);
+            if (c == ':') { // remove optional colon
+                // remove colon
+                StringBuilder sb = new StringBuilder(dateStr);
+                sb.delete(len-3, len-2);
+                dateStr = sb.toString();
+            } else if (c == '+' || c == '-') { // missing minutes
+                // let's just append '00'
+                dateStr += "00";
+            }
+            // otherwise regular (or invalid)
+
+            df = _formatISO8601;
+            if (_formatISO8601 == null) {
+                df = _formatISO8601 = (SimpleDateFormat) DATE_FORMAT_ISO8601.clone();
+            }
         }
-        // Fall back to RFC-1123
+        return df.parse(dateStr, pos);
+    }
+
+    protected Date parseAsRFC1123(String dateStr, ParsePosition pos)
+    {
         if (_formatRFC1123 == null) {
-            _formatRFC1123 = (DateFormat) DATE_FORMAT_RFC1123.clone();
+            _formatRFC1123 = (SimpleDateFormat) DATE_FORMAT_RFC1123.clone();
         }
-        return _formatRFC1123;
+        return _formatRFC1123.parse(dateStr, pos);
     }
 }
 
