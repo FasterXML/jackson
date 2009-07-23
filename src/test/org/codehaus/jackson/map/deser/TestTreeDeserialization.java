3df3e0380ce4b94f72cca6ab67671ff19a72f072
Tatu Saloranta <tatu@fasterxml.com>
2009-07-23 04:33:29 +0000

    Fixed [JACKSON-143]

--- src/test/org/codehaus/jackson/map/deser/TestTreeDeserialization.java
+++ src/test/org/codehaus/jackson/map/deser/TestTreeDeserialization.java
@@ -40,4 +40,43 @@ public class TestTreeDeserialization
         ObjectNode on = (ObjectNode) n;
         assertEquals(3, on.get("a").getIntValue());
     }
+
+    /// Verifying [JACKSON-143]
+    public void testArrayNodeEquality()
+    {
+        ArrayNode n1 = new ArrayNode(null);
+        ArrayNode n2 = new ArrayNode(null);
+
+        assertTrue(n1.equals(n2));
+        assertTrue(n2.equals(n1));
+
+        n1.add(TextNode.valueOf("Test"));
+
+        assertFalse(n1.equals(n2));
+        assertFalse(n2.equals(n1));
+
+        n2.add(TextNode.valueOf("Test"));
+
+        assertTrue(n1.equals(n2));
+        assertTrue(n2.equals(n1));
+    }
+
+    public void testObjectNodeEquality()
+    {
+        ObjectNode n1 = new ObjectNode(null);
+        ObjectNode n2 = new ObjectNode(null);
+
+        assertTrue(n1.equals(n2));
+        assertTrue(n2.equals(n1));
+
+        n1.put("x", TextNode.valueOf("Test"));
+
+        assertFalse(n1.equals(n2));
+        assertFalse(n2.equals(n1));
+
+        n2.put("x", TextNode.valueOf("Test"));
+
+        assertTrue(n1.equals(n2));
+        assertTrue(n2.equals(n1));
+    }
 }
