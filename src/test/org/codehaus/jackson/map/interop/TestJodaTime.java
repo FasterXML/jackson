aef00d42eb89c1629a9e8591277cab0396a0b8ea
Tatu Saloranta <tatu@fasterxml.com>
2009-09-05 05:15:06 +0000

    Added simple unit test to see if Joda DateTime can be serialized.

---
+++ src/test/org/codehaus/jackson/map/interop/TestJodaTime.java
@@ -0,0 +1,38 @@
+package org.codehaus.jackson.map.interop;
+
+import org.codehaus.jackson.map.BaseMapTest;
+
+import java.io.*;
+import java.util.*;
+
+import org.codehaus.jackson.*;
+import org.codehaus.jackson.annotate.*;
+import org.codehaus.jackson.map.*;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+/**
+ * Unit tests for verifying limited interoperability for Joda time
+ * (minimal just because Jackson tries to minimize default deps to
+ * all non-JDK libs -- Joda is a fine date/time lib, and ideally
+ * better integration should be added via extension mechanisms)
+ */
+public class TestJodaTime
+    extends org.codehaus.jackson.map.BaseMapTest
+{
+    /**
+     * First: let's ensure that serialization does not fail
+     * with an error (see [JACKSON-157]).
+     */
+    public void testSerialization() throws IOException
+    {
+        ObjectMapper m = new ObjectMapper();
+        // let's use epoch time (Jan 1, 1970, UTC)
+        DateTime dt = new DateTime(0L, DateTimeZone.UTC);
+        Map<String,Object> result = writeAndMap(m, dt);
+
+        // should verify something here? looks like we get 24 fields...
+        assertEquals(24, result.size());
+    }
+}
