3b91d94b977e09899da0aa7fa73817c95ae9158b
Tatu Saloranta <tatu@fasterxml.com>
2009-09-10 06:50:43 +0000

    ...

--- src/mapper/java/org/codehaus/jackson/map/ser/BeanSerializer.java
+++ src/mapper/java/org/codehaus/jackson/map/ser/BeanSerializer.java
@@ -1,7 +1,6 @@
 package org.codehaus.jackson.map.ser;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.util.Collection;
@@ -12,7 +11,6 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.schema.SchemaAware;
 import org.codehaus.jackson.schema.JsonSchema;
 import org.codehaus.jackson.node.ObjectNode;
-import org.codehaus.jackson.node.JsonNodeFactory;
 import org.codehaus.jackson.map.*;
 
 /**
@@ -24,7 +22,7 @@ import org.codehaus.jackson.map.*;
  * otherwise we could end up with an infinite loop.
  */
 public class BeanSerializer
-    extends JsonSerializer<Object>
+    extends SerializerBase<Object>
     implements ResolvableSerializer, SchemaAware
 {
     final protected String _className;
@@ -58,23 +56,8 @@ public class BeanSerializer
                 _props[i].serializeAsField(bean, jgen, provider);
             }
             jgen.writeEndObject();
-        } catch (IOException ioe) {
-            // first: IOExceptions (and sub-classes) to be passed as is
-            throw ioe;
         } catch (Exception e) {
-            // [JACKSON-55] Need to add reference information
-            /* 05-Mar-2009, tatu: But one nasty edge is when we get
-             *   StackOverflow: usually due to infinite loop. But that
-             *   usually gets hidden within an InvocationTargetException...
-             */
-            Throwable t = e;
-            while (t instanceof InvocationTargetException && t.getCause() != null) {
-                t = t.getCause();
-            }
-            if (t instanceof Error) {
-                throw (Error) t;
-            }
-            throw JsonMappingException.wrapWithPath(t, bean, _props[i].getName());
+            wrapAndThrow(e, bean, _props[i].getName());
         } catch (StackOverflowError e) {
             /* 04-Sep-2009, tatu: Dealing with this is tricky, since we do not
              *   have many stack frames to spare... just one or two; can't
@@ -90,11 +73,9 @@ public class BeanSerializer
     public JsonNode getSchema(SerializerProvider provider, Type typeHint)
             throws JsonMappingException
     {
-        ObjectNode o = JsonNodeFactory.instance.objectNode();
+        ObjectNode o = createSchemaNode("object", true);
         //todo: should the classname go in the title?
         //o.put("title", _className);
-        o.put("type", "object");
-        o.put("optional", true);
         ObjectNode propertiesNode = o.objectNode();
         for (int i = 0; i < _props.length; i++) {
             BeanPropertyWriter prop = _props[i];
