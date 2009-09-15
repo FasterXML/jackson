2e1443a2c9c3ca4597bff6f18576c3749393d112
Tatu Saloranta <tatu@fasterxml.com>
2009-09-15 00:15:16 +0000

    Cleaning up generics for enum resolver (note: need to clean up new compilation warnings, resulting from this cleanup)

--- src/mapper/java/org/codehaus/jackson/map/deser/EnumResolver.java
+++ src/mapper/java/org/codehaus/jackson/map/deser/EnumResolver.java
@@ -8,43 +8,40 @@ import java.util.*;
  * Helper class used to resolve String values (either Json Object field
  * names or regular String values) into Java Enum instances.
  */
-public final class EnumResolver
+public final class EnumResolver<T extends Enum<T>>
 {
-    protected final Class<Enum<?>> _enumClass;
+    protected final Class<T> _enumClass;
 
-    protected final Enum<?>[] _enums;
+    protected final T[] _enums;
 
     /**
      *
      */
-    protected final HashMap<String, Enum<?>> _enumsById;
+    protected final HashMap<String, T> _enumsById;
 
-    private EnumResolver(Class<Enum<?>> enumClass, Enum<?>[] enums, HashMap<String, Enum<?>> map)
+    private EnumResolver(Class<T> enumClass, T[] enums, HashMap<String, T> map)
     {
         _enumClass = enumClass;
         _enums = enums;
         _enumsById = map;
     }
 
-    public static EnumResolver constructFor(Class<?> rawEnumCls, AnnotationIntrospector ai)
+    public static <ET extends Enum<ET>> EnumResolver constructFor(Class<ET> enumCls, AnnotationIntrospector ai)
     {
-        @SuppressWarnings("unchecked")
-        Class<Enum<?>> enumCls = (Class<Enum<?>>) rawEnumCls;
-
-        Enum<?>[] enumValues = enumCls.getEnumConstants();
-        HashMap<String, Enum<?>> map = new HashMap<String, Enum<?>>();
-        for (Enum<?> e : enumValues) {
+        T[] enumValues = enumCls.getEnumConstants();
+        HashMap<String, ET> map = new HashMap<String, ET>();
+        for (ET e : enumValues) {
             map.put(ai.findEnumValue(e), e);
         }
-        return new EnumResolver(enumCls, enumValues, map);
+        return new EnumResolver<ET>(enumCls, enumValues, map);
     }
 
-    public Enum<?> findEnum(String key)
+    public T findEnum(String key)
     {
         return _enumsById.get(key);
     }
 
-    public Enum<?> getEnum(int index)
+    public T getEnum(int index)
     {
         if (index < 0 || index >= _enums.length) {
             return null;
@@ -52,7 +49,7 @@ public final class EnumResolver
         return _enums[index];
     }
 
-    public Class<Enum<?>> getEnumClass() { return _enumClass; }
+    public Class<T> getEnumClass() { return _enumClass; }
 
     public int lastValidIndex() { return _enums.length-1; }
 }
