689ca790910633dbfaebce6a62fad9670860fefb
Tatu Saloranta <tatu@fasterxml.com>
2010-02-19 07:09:41 +0000

    Moved failing unit test (for bug that won't be fixed for 1.5) in sandbox, until bug will be worked on

---
+++ src/test/org/codehaus/jackson/map/jsontype/TestDefaultForLists.java
@@ -0,0 +1,68 @@
+package org.codehaus.jackson.map.jsontype;
+
+import java.util.*;
+
+import org.codehaus.jackson.map.*;
+
+public class TestDefaultForLists
+    extends BaseMapTest
+{
+    /**
+     * Wrapper bean needed before there is a way to force
+     * type of the root value. Long is used because it is a final
+     * type, but not one of "untypable" ones.
+     */
+    static class ListOfLongs {
+        public List<Long> longs;
+
+        public ListOfLongs() { }
+        public ListOfLongs(Long ... l) {
+            longs = Arrays.asList(l);
+        }
+    }
+
+    static class ListOfNumbers {
+        public List<Number> longs;
+
+        public ListOfNumbers() { }
+        public ListOfNumbers(Number ... l) {
+            longs = Arrays.asList(l);
+        }
+    }
+
+    public void testListOfLongs() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+        m.enableDefaultTyping();
+        ListOfLongs input = new ListOfLongs(1L, 2L, 3L);
+        String str = m.writeValueAsString(input);
+        ListOfLongs output = m.readValue(str, ListOfLongs.class);
+
+        assertNotNull(output.longs);
+        assertEquals(3, output.longs.size());
+        assertEquals(Long.valueOf(1L), output.longs.get(0));
+        assertEquals(Long.valueOf(2L), output.longs.get(1));
+        assertEquals(Long.valueOf(3L), output.longs.get(2));
+    }
+
+    /**
+     * Then bit more heterogenous list; also tests mixing of
+     * regular scalar types, and non-typed ones (int and double
+     * will never have type info added; other numbers will if
+     * necessary)
+     */
+    public void testListOfNumbers() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+        m.enableDefaultTyping();
+        ListOfNumbers input = new ListOfNumbers(Long.valueOf(1L), Integer.valueOf(2), Double.valueOf(3.0));
+        String str = m.writeValueAsString(input);
+        ListOfNumbers output = m.readValue(str, ListOfNumbers.class);
+
+        assertNotNull(output.longs);
+        assertEquals(3, output.longs.size());
+        assertEquals(Long.valueOf(1L), output.longs.get(0));
+        assertEquals(Integer.valueOf(2), output.longs.get(1));
+        assertEquals(Double.valueOf(3.0), output.longs.get(2));
+    }
+}
