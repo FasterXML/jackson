bcf0b6f9d027c57b7671bccc5be64962afbf3814
Tatu Saloranta <tatu@fasterxml.com>
2010-07-01 23:56:48 +0000

    Added unit test to check support (or lack thereof) for google collections (1.0)

---
+++ src/test/org/codehaus/jackson/map/interop/TestGoogleCollections.java
@@ -0,0 +1,18 @@
+package org.codehaus.jackson.map.interop;
+
+import org.codehaus.jackson.map.*;
+
+import com.google.common.collect.*;
+
+public class TestGoogleCollections
+    extends org.codehaus.jackson.map.BaseMapTest
+{
+    public void testTrivialMultiMapSerialize() throws Exception
+    {
+        Multimap<String,String> map = HashMultimap.create();
+        map.put("a", "1");
+        String json = new ObjectMapper().writeValueAsString(map);
+        assertNotNull(json);
+        assertEquals("{\"a\":[\"1\"]}", json);
+    }
+}
