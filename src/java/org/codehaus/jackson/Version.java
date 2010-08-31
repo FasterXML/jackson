42e9b50a5206129c639d8d43dd3409df6e052ac2
Tatu Saloranta <tatu@fasterxml.com>
2010-08-31 06:14:14 +0000

    Initial baseline work to get [JACKSON-278] (exposing version information via API) implemented for 1.6.0

---
+++ src/java/org/codehaus/jackson/Version.java
@@ -0,0 +1,74 @@
+package org.codehaus.jackson;
+
+/**
+ * Object that encapsulates version information of a component,
+ * and is return by {@link Versioned#version}.
+ * 
+ * @since 1.6
+ */
+public class Version
+    implements Comparable<Version>
+{
+    public final static Version UNKNOWN_VERSION = new Version(0, 0, 0);
+
+    protected final int _majorVersion;
+
+    protected final int _minorVersion;
+
+    protected final int _patchLevel;
+    
+    public Version(int major, int minor, int patchLevel)
+    {
+        _majorVersion = major;
+        _minorVersion = minor;
+        _patchLevel = patchLevel;
+    }
+
+    public Version unknownVersion() { return UNKNOWN_VERSION; }
+
+    public boolean isUknownVersion() { return (this == UNKNOWN_VERSION); }
+    
+    public int getMajorVersion() { return _majorVersion; }
+    public int getMinorVersion() { return _minorVersion; }
+    public int getPatchLevel() { return _patchLevel; }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(_majorVersion).append('.');
+        sb.append(_minorVersion).append('.');
+        sb.append(_patchLevel);
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return _majorVersion + _minorVersion + _patchLevel;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == this) return true;
+        if (o == null) return false;
+        if (o.getClass() != getClass()) return false;
+        Version other = (Version) o;
+        return (other._majorVersion == _majorVersion)
+            && (other._minorVersion == _minorVersion)
+            && (other._patchLevel == _patchLevel);
+    }
+
+    @Override
+    public int compareTo(Version other)
+    {
+        int diff = _majorVersion - other._majorVersion;
+        if (diff == 0) {
+            diff = _minorVersion - other._minorVersion;
+            if (diff == 0) {
+                diff = _patchLevel - other._patchLevel;
+            }
+        }
+        return diff;
+    }
+}
---
+++ src/java/org/codehaus/jackson/util/VersionUtil.java
@@ -0,0 +1,13 @@
+package org.codehaus.jackson.util;
+
+import org.codehaus.jackson.Version;
+
+/**
+ * Functionality for supporting exposing of component {@link Version}s.
+ * 
+ * @since 1.6
+ */
+public class VersionUtil
+{
+
+}
