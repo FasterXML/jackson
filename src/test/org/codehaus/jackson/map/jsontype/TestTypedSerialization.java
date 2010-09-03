1f2a57e61992bddd0aedb0a1bdf512892bb8e188
Tatu Saloranta <tatu@fasterxml.com>
2010-09-03 06:00:04 +0000

    Added unit test for [JACKSON-356]

--- src/test/org/codehaus/jackson/map/jsontype/TestTypedSerialization.java
+++ src/test/org/codehaus/jackson/map/jsontype/TestTypedSerialization.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.annotate.JsonTypeName;
 import org.codehaus.jackson.map.*;
+import org.codehaus.jackson.type.TypeReference;
 
 import static org.codehaus.jackson.annotate.JsonTypeInfo.*;
 
@@ -62,7 +63,34 @@ public class TestTypedSerialization
 
     @JsonTypeInfo(use=Id.CLASS, include=As.WRAPPER_ARRAY)
     interface TypeWithArray { }
-    
+
+    // [JACKSON-356]
+    public static class JSONResponse<T> {
+
+        private T result;
+
+        public T getResult() {
+            return result;
+        }
+
+        public void setResult(T result) {
+            this.result = result;
+        }
+    } 
+
+    @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
+    public static class Parent {
+        public String parentContent = "PARENT";
+    }
+
+    public static class Child1 extends Parent {
+        public String childContent1 = "CHILD1";
+    }
+
+    public static class Child2 extends Parent {
+        public String childContent2 = "CHILD2";
+    }
+
     /*
     /**********************************************************
     /* Unit tests
@@ -127,7 +155,7 @@ public class TestTypedSerialization
     /* !!! 30-Jan-2010, tatus: I am not completely sure below works as it should
      *    Problem is, context of "untyped" map should prevent type information
      *    being added to Animal entries, because Object.class has no type.
-     *    If type information is inluded, it will not be useful for deserialization,
+     *    If type information is included, it will not be useful for deserialization,
      *    since static type does not carry through (unlike in serialization).
      *    
      *    But it is not quite clear how type information should be pushed through
@@ -162,6 +190,37 @@ public class TestTypedSerialization
         Map<?,?> a2 = (Map<?,?>) l.get(1);
         assertEquals(3, a2.size());
         assertEquals(Dog.class.getName(), a2.get(classProp));
+    }
+
+    public void testSubTypesFor356() throws Exception
+    {
+        JSONResponse<List<Parent>> input = new JSONResponse<List<Parent>>();
+
+        List<Parent> embedded = new ArrayList<Parent>();
+        embedded.add(new Child1());
+        embedded.add(new Child2());
+        input.setResult(embedded);
+        ObjectMapper mapper = new ObjectMapper();
+        byte[] json = mapper.writeValueAsBytes(input);
+
+        //System.out.println("After Serialization: " + new String(json));
+
+        TypeReference<?> type = new TypeReference<JSONResponse<List<Parent>>>() { };
+        JSONResponse<List<Parent>> out = mapper.readValue(json, 0, json.length, type);
+
+        List<Parent> deserializedContent = (List<Parent>) out.getResult();
+
+        assertEquals(2, deserializedContent.size());
+        assertTrue(deserializedContent.get(0) instanceof Parent);
+        assertTrue(deserializedContent.get(0) instanceof Child1);
+        assertFalse(deserializedContent.get(0) instanceof Child2);
+        assertTrue(deserializedContent.get(1) instanceof Child2);
+        assertFalse(deserializedContent.get(1) instanceof Child1);
+
+        assertEquals("PARENT", ((Child1) deserializedContent.get(0)).parentContent);
+        assertEquals("PARENT", ((Child1) deserializedContent.get(1)).parentContent);
+        assertEquals("CHILD1", ((Child1) deserializedContent.get(0)).childContent1);
+        assertEquals("CHILD2", ((Child2) deserializedContent.get(1)).childContent2);
     }
 }
 
