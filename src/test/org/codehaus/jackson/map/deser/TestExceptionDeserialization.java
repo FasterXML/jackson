e5ffe2ed61707d2a7306cfa0de472163e931072a
Tatu Saloranta <tatu@fasterxml.com>
2010-09-30 19:08:17 +0000

    Fixed [JACKSON-377], ThrowableDeserializer not correctly handling @JsonCreator or @JsonAnySetter

--- src/test/org/codehaus/jackson/map/deser/TestExceptionDeserialization.java
+++ src/test/org/codehaus/jackson/map/deser/TestExceptionDeserialization.java
@@ -3,7 +3,11 @@ package org.codehaus.jackson.map.deser;
 import org.codehaus.jackson.map.BaseMapTest;
 
 import java.io.IOException;
+import java.util.*;
 
+import org.codehaus.jackson.annotate.JsonAnySetter;
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.*;
 
 /**
@@ -12,8 +16,39 @@ import org.codehaus.jackson.map.*;
 public class TestExceptionDeserialization
     extends BaseMapTest
 {
-    public void testIOException()
-        throws IOException
+    @SuppressWarnings("serial")
+    static class MyException extends Exception
+    {
+        protected int value;
+
+        protected String myMessage;
+        protected HashMap<String,Object> stuff = new HashMap<String, Object>();
+        
+        @JsonCreator
+        MyException(@JsonProperty("message") String msg, @JsonProperty("value") int v)
+        {
+            super(msg);
+            myMessage = msg;
+            value = v;
+        }
+
+        public int getValue() { return value; }
+        
+        public String getFoo() { return "bar"; }
+
+        @JsonAnySetter public void setter(String key, Object value)
+        {
+            stuff.put(key, value);
+        }
+    }
+    
+    /*
+    /**********************************************************
+    /* Tests
+    /**********************************************************
+     */
+
+    public void testIOException() throws IOException
     {
         ObjectMapper mapper = new ObjectMapper();
         IOException ioe = new IOException("TEST");
@@ -23,5 +58,19 @@ public class TestExceptionDeserialization
         IOException result = mapper.readValue(json, IOException.class);
 
         assertEquals(ioe.getMessage(), result.getMessage());
+    }
+
+    // As per [JACKSON-377]
+    public void testWithCreator() throws IOException
+    {
+        final String MSG = "the message";
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(new MyException(MSG, 3));
+
+        MyException result = mapper.readValue(json, MyException.class);
+        assertEquals(MSG, result.getMessage());
+        assertEquals(3, result.value);
+        assertEquals(1, result.stuff.size());
+        assertEquals(result.getFoo(), result.stuff.get("foo"));
     }
 }
