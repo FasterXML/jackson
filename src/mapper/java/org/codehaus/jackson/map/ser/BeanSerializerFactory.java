701b928f98acae90e7d99006d759da40101bde8c
Tatu Saloranta <tatu@fasterxml.com>
2010-10-17 02:53:33 +0000

    Fix [JACKSON-392] in trunk

--- src/mapper/java/org/codehaus/jackson/map/ser/BeanSerializerFactory.java
+++ src/mapper/java/org/codehaus/jackson/map/ser/BeanSerializerFactory.java
@@ -219,32 +219,36 @@ public class BeanSerializerFactory
         
         // First: any detectable (auto-detect, annotations) properties to serialize?
         List<BeanPropertyWriter> props = findBeanProperties(config, beanDesc);
+        AnnotatedMethod anyGetter = beanDesc.findAnyGetter();
+        // No properties, no serializer
+        // 16-Oct-2010, tatu: Except that @JsonAnyGetter needs to count as getter
         if (props == null || props.size() == 0) {
-            // No properties, no serializer
-            /* 27-Nov-2009, tatu: Except that as per [JACKSON-201], we are
-             *   ok with that as long as it has a recognized class annotation
-             *  (which may come from a mix-in too)
-             */
-            if (beanDesc.hasKnownClassAnnotations()) {
-                return BeanSerializer.createDummy(beanDesc.getBeanClass());
+            if (anyGetter == null) {
+                /* 27-Nov-2009, tatu: Except that as per [JACKSON-201], we are
+                 *   ok with that as long as it has a recognized class annotation
+                 *  (which may come from a mix-in too)
+                 */
+                if (beanDesc.hasKnownClassAnnotations()) {
+                    return BeanSerializer.createDummy(beanDesc.getBeanClass());
+                }
+                return null;
             }
-            return null;
+            props = Collections.emptyList();
+        } else {
+            // Any properties to suppress?
+            props = filterBeanProperties(config, beanDesc, props);
+            // Do they need to be sorted in some special way?
+            props = sortBeanProperties(config, beanDesc, props);
         }
-        // Any properties to suppress?
-        props = filterBeanProperties(config, beanDesc, props);
-        // Do they need to be sorted in some special way?
-        props = sortBeanProperties(config, beanDesc, props);
         BeanSerializer ser = new BeanSerializer(beanDesc.getBeanClass(), props);
-        // 1.6: any-setter?
-        AnnotatedMethod m = beanDesc.findAnyGetter();
-        if (m != null) {
-            JavaType type = m.getType(beanDesc.bindingsForBeanType());
+        if (anyGetter != null) { // since 1.6
+            JavaType type = anyGetter.getType(beanDesc.bindingsForBeanType());
             // copied from BasicSerializerFactory.buildMapSerializer():
             boolean staticTyping = config.isEnabled(SerializationConfig.Feature.USE_STATIC_TYPING);
             JavaType valueType = type.getContentType();
             TypeSerializer typeSer = createTypeSerializer(valueType, config);
             MapSerializer mapSer = MapSerializer.construct(/* ignored props*/ null, type, staticTyping, typeSer);
-            ser.setAnyGetter(new AnyGetterWriter(m, mapSer));
+            ser.setAnyGetter(new AnyGetterWriter(anyGetter, mapSer));
         }
         
         // One more thing: need to gather view information, if any:
