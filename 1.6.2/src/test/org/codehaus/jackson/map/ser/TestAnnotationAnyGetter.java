1f13f9c16e1517d1febab61cf8253f08de7b314f
Tatu Saloranta <tatu@fasterxml.com>
2010-10-17 02:56:48 +0000

    Fixed [JACKSON-392] -- beans with just @JsonAnySetter failing -- for 1.6.2

--- 1.6.2/src/test/org/codehaus/jackson/map/ser/TestAnnotationAnyGetter.java
+++ 1.6.2/src/test/org/codehaus/jackson/map/ser/TestAnnotationAnyGetter.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.codehaus.jackson.annotate.JsonAnyGetter;
 import org.codehaus.jackson.map.BaseMapTest;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 
 public class TestAnnotationAnyGetter
     extends BaseMapTest
@@ -27,6 +28,16 @@ public class TestAnnotationAnyGetter
         @JsonAnyGetter
         public Map<String,Boolean> getExtra() { return extra; }
     }
+
+    static class AnyOnlyBean
+    {
+        @JsonAnyGetter
+        public Map<String,Integer> any() {
+            HashMap<String,Integer> map = new HashMap<String,Integer>();
+            map.put("a", 3);
+            return map;
+        }
+    }
     
     /*
     /*********************************************************
@@ -44,4 +55,22 @@ public class TestAnnotationAnyGetter
         assertEquals(Boolean.TRUE, map.get("a"));
     }
 
+    // [JACKSON-392]
+    public void testAnyOnly() throws Exception
+    {
+        ObjectMapper m;
+
+        // First, with normal fail settings:
+        m = new ObjectMapper();
+        m.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, true);
+        String json = serializeAsString(m, new AnyOnlyBean());
+        assertEquals("{\"a\":3}", json);
+
+        // then without fail
+        m = new ObjectMapper();
+        m.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false);
+        json = serializeAsString(m, new AnyOnlyBean());
+        assertEquals("{\"a\":3}", json);
+    }
+    
 }
