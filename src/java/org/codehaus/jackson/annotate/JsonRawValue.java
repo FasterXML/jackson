3c6224568237c4eec3c964ffad42fcdcdfc52c5a
Tatu Saloranta <tatu@fasterxml.com>
2010-10-19 06:29:41 +0000

    ...

--- src/java/org/codehaus/jackson/annotate/JsonRawValue.java
+++ src/java/org/codehaus/jackson/annotate/JsonRawValue.java
@@ -6,17 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marker annotation
- * that indicates that the annotated method or field 
- * should be serialized as a raw value (using toString() method)
- * without being quoted. 
- * 
- * <p>
+ * Marker annotation that indicates that the annotated method
+ * or field should be serialized by including literal String value
+ * of the property as is, without quoting of characters.
  * This can be useful for injecting values already serialized in JSON or 
  * passing javascript function definitions from server to a javascript client.
+ *<p>
+ * Warning: the resulting JSON stream may be invalid depending on your input value.
  * 
- * <p>Warning: the resulting JSON stream may be invalid depending on your input value.
- * 
+ * @since 1.7.0
  */
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
