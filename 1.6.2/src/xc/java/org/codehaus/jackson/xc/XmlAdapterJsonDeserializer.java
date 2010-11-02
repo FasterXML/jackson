58f11506b13f9bd03071fa93314d8b2b1f2a73a2
Tatu Saloranta <tatu@fasterxml.com>
2010-11-02 05:08:32 +0000

    Fixed [JACKSON-404]

--- 1.6.2/src/xc/java/org/codehaus/jackson/xc/XmlAdapterJsonDeserializer.java
+++ 1.6.2/src/xc/java/org/codehaus/jackson/xc/XmlAdapterJsonDeserializer.java
@@ -1,51 +1,59 @@
 package org.codehaus.jackson.xc;
 
+import java.io.IOException;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.map.DeserializationContext;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.TypeDeserializer;
+import org.codehaus.jackson.map.*;
 import org.codehaus.jackson.map.deser.StdDeserializer;
-
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-import java.io.IOException;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import org.codehaus.jackson.map.type.TypeFactory;
+import org.codehaus.jackson.type.JavaType;
 
 /**
  * @author Ryan Heaton
+ * @author Tatu Saloranta
  */
 public class XmlAdapterJsonDeserializer
     extends StdDeserializer<Object>
 {
-    private final XmlAdapter<Object,Object> xmlAdapter;
-    private final Class<?> valueClass;
+    protected final static JavaType ADAPTER_TYPE = TypeFactory.type(XmlAdapter.class);
+    
+    protected final XmlAdapter<Object,Object> _xmlAdapter;
+    protected final JavaType _valueType;
 
+    protected JsonDeserializer<?> _deserializer;
+    
     public XmlAdapterJsonDeserializer(XmlAdapter<Object,Object> xmlAdapter)
     {
-        super(Object.class); // type not really known, not passed
-        this.xmlAdapter = xmlAdapter;
-        this.valueClass = findValueClass();
-    }
-
-    private Class<?> findValueClass()
-    {
-        Type superClass = this.xmlAdapter.getClass().getGenericSuperclass();
-        while (superClass instanceof ParameterizedType && XmlAdapter.class != ((ParameterizedType)superClass).getRawType()) {
-            superClass = ((Class<?>) ((ParameterizedType) superClass).getRawType()).getGenericSuperclass();
-        }
-        return (Class<?>) ((ParameterizedType) superClass).getActualTypeArguments()[0];
+        super(Object.class); // type not yet known (will be in a second), but that's ok...
+        _xmlAdapter = xmlAdapter;
+        /* [JACKSON-404] Need to figure out generic type parameters
+         *   used...
+         */
+        JavaType type = TypeFactory.type(xmlAdapter.getClass());
+        JavaType[] rawTypes = TypeFactory.findParameterTypes(type, XmlAdapter.class);
+        _valueType = (rawTypes == null || rawTypes.length == 0)  ? TypeFactory.type(Object.class) : rawTypes[0];
     }
 
     @Override
     public Object deserialize(JsonParser jp, DeserializationContext ctxt)
         throws IOException, JsonProcessingException
     {
-        Object boundObject = jp.readValueAs(valueClass);
+        /* Unfortunately we can not use the usual resolution mechanism (ResolvableDeserializer)
+         * because it won't get called due to way adapters are created. So, need to do it
+         * lazily when we get here:
+         */
+        JsonDeserializer<?> deser = _deserializer;
+        if (deser == null) {
+            _deserializer = deser = ctxt.getDeserializerProvider().findValueDeserializer
+                (ctxt.getConfig(), _valueType, ADAPTER_TYPE, "");
+        }
+        Object boundObject = deser.deserialize(jp, ctxt);
         try {
-            return this.xmlAdapter.unmarshal(boundObject);
+            return _xmlAdapter.unmarshal(boundObject);
         } catch (Exception e) {
-            throw new JsonMappingException("Unable to unmarshal: "+e.getMessage(), e);
+            throw new JsonMappingException("Unable to unmarshal (to type "+_valueType+"): "+e.getMessage(), e);
         }
     }
 
