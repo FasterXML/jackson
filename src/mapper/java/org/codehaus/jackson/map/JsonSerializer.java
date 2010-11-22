ed0c73fa9735fbd9b954c353c607e7581fac2ab3
Tatu Saloranta <tatu@fasterxml.com>
2010-11-22 02:34:13 +0000

    ...

--- src/mapper/java/org/codehaus/jackson/map/JsonSerializer.java
+++ src/mapper/java/org/codehaus/jackson/map/JsonSerializer.java
@@ -62,10 +62,14 @@ public abstract class JsonSerializer<T>
     
     /**
      * Method for accessing type of Objects this serializer can handle.
+     * Note that this information is not guaranteed to be exact -- it
+     * may be a more generic (super-type) -- but it should not be
+     * incorrect (return a non-related type).
      *<p>
      * Default implementation will return null, which essentially means
      * same as returning <code>Object.class</code> would; that is, that
      * nothing is known about handled type.
+     *<p>
      */
     public Class<T> handledType() { return null; }
     
