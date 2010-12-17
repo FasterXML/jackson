77cb2301dfffd921acc904115812cfe343ea6464
Tatu Saloranta <tatu@fasterxml.com>
2010-12-17 05:48:23 +0000

    Add unit test to reproduce [JACKSON-428]

--- src/test/org/codehaus/jackson/map/jsontype/TestDefaultForMaps.java
+++ src/test/org/codehaus/jackson/map/jsontype/TestDefaultForMaps.java
@@ -2,8 +2,11 @@ package org.codehaus.jackson.map.jsontype;
 
 import java.util.*;
 
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.map.*;
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.jsontype.impl.TypeNameIdResolver;
+import org.codehaus.jackson.map.type.TypeFactory;
 
 public class TestDefaultForMaps 
     extends BaseMapTest
@@ -31,7 +34,7 @@ public class TestDefaultForMaps
     static class MapHolder
     {
         @JsonDeserialize(keyAs=MapKey.class, keyUsing=MapKeyDeserializer.class)
-        public Map<MapKey,List<Integer>> map;
+        public Map<MapKey,List<Object>> map;
     }
 
     /*
@@ -43,15 +46,51 @@ public class TestDefaultForMaps
     // !!!! Incomplete: does not reproduce issue, yet
     public void testJackson428() throws Exception
     {
-//        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper serMapper = new ObjectMapper();
 
-        /*
-        TypeResolverBuilder<?> serializerTyper = new DefaultTypeResolverBuilder(DefaultTyping.NON_FINAL);
+        TypeResolverBuilder<?> serializerTyper = new ObjectMapper.DefaultTypeResolverBuilder(ObjectMapper.DefaultTyping.NON_FINAL);
         serializerTyper = serializerTyper.init(JsonTypeInfo.Id.NAME, createTypeNameIdResolver(true));
         serializerTyper = serializerTyper.inclusion(JsonTypeInfo.As.PROPERTY);
-        mapper.setDefaultTyping(serializerTyper);
+        serMapper.setDefaultTyping(serializerTyper);
 
-        MapHolder result = mapper.readValue("{\"map\":{}}", MapHolder.class);
-        */
+        // Let's start by constructing something to serialize first
+        MapHolder holder = new MapHolder();
+        holder.map = new HashMap<MapKey,List<Object>>();;
+        List<Object> ints = new ArrayList<Object>();
+        ints.add(Integer.valueOf(3));
+        holder.map.put(new MapKey("key"), ints);
+        String json = serMapper.writeValueAsString(holder);
+
+//System.out.println("JSON == "+json);        
+
+        // Then deserialize: need separate mapper to initialize type id resolver appropriately
+        ObjectMapper deserMapper = new ObjectMapper();
+        TypeResolverBuilder<?> deserializerTyper = new ObjectMapper.DefaultTypeResolverBuilder(ObjectMapper.DefaultTyping.NON_FINAL);
+        deserializerTyper = deserializerTyper.init(JsonTypeInfo.Id.NAME, createTypeNameIdResolver(false));
+        deserializerTyper = deserializerTyper.inclusion(JsonTypeInfo.As.PROPERTY);
+        deserMapper.setDefaultTyping(deserializerTyper);
+
+        MapHolder result = deserMapper.readValue(json, MapHolder.class);
+        assertNotNull(result);
+        Map<?,?> map = result.map;
+        assertEquals(1, map.size());
+        Map.Entry<?,?> entry = map.entrySet().iterator().next();
+        Object key = entry.getKey();
+        assertEquals(MapKey.class, key.getClass());
+        Object value = entry.getValue();
+        assertTrue(value instanceof List<?>);
+        List<?> list = (List<?>) value;
+        assertEquals(1, list.size());
+        assertEquals(Integer.class, list.get(0).getClass());
+        assertEquals(Integer.valueOf(3), list.get(0));
+    }
+
+    protected TypeNameIdResolver createTypeNameIdResolver(boolean forSerialization)
+    {
+        Collection<NamedType> subtypes = new ArrayList<NamedType>();
+        subtypes.add(new NamedType(MapHolder.class, "mapHolder"));
+        subtypes.add(new NamedType(ArrayList.class, "AList"));
+        subtypes.add(new NamedType(HashMap.class, "HMap"));
+        return TypeNameIdResolver.construct(TypeFactory.type(Object.class), subtypes, forSerialization, !forSerialization);
     }
 }
