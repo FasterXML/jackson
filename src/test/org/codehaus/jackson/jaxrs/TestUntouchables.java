720a842f3ce7bb23faa9075cd1f9495bc981e2ad
Tatu Saloranta <tatu@fasterxml.com>
2011-01-14 00:57:28 +0000

    Add unit test for [JACKSON-460], ensuring that isJsonType remains overridable

--- src/test/org/codehaus/jackson/jaxrs/TestUntouchables.java
+++ src/test/org/codehaus/jackson/jaxrs/TestUntouchables.java
@@ -2,14 +2,36 @@ package org.codehaus.jackson.jaxrs;
 
 import java.util.*;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.StreamingOutput;
 
 /**
+ * Unit tests for verifying that certain JDK base types will be
+ * ignored by default Jackson JAX-RS conversion provider.
+ * 
  * @since 1.5
  */
 public class TestUntouchables
     extends main.BaseTest
 {
+    /**
+     * Test type added for [JACKSON-460]... just to ensure that "isJsonType"
+     * remains overridable.
+     */
+    public static class MyJacksonJsonProvider extends JacksonJsonProvider {
+         // ensure isJsonType remains "protected" Ð this is a compile-time check.
+         // Some users of JacksonJsonProvider override this method;
+         // changing to "private" would regress them.
+         @Override
+         protected boolean isJsonType(MediaType mediaType) { return super.isJsonType(mediaType); }
+    }
+
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+    
     public void testDefaultUntouchables() throws Exception
     {
         JacksonJsonProvider prov = new JacksonJsonProvider();
