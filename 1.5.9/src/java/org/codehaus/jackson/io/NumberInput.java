40cf1b9625b933711e0e3572e970e74ddfaaa939
Tatu Saloranta <tatu@fasterxml.com>
2011-02-11 22:34:20 +0000

    Fix [JACKSON-491], bug in long number overflow detection code

--- 1.5.9/src/java/org/codehaus/jackson/io/NumberInput.java
+++ 1.5.9/src/java/org/codehaus/jackson/io/NumberInput.java
@@ -75,8 +75,9 @@ public final class NumberInput
         if (len > cmpLen) return false;
 
         for (int i = 0; i < cmpLen; ++i) {
-            if (digitChars[offset+i] > cmpStr.charAt(i)) {
-                return false;
+            int diff = digitChars[offset+i] - cmpStr.charAt(i);
+            if (diff != 0) {
+                return (diff < 0);
             }
         }
         return true;
@@ -101,8 +102,9 @@ public final class NumberInput
 
         // could perhaps just use String.compareTo()?
         for (int i = 0; i < cmpLen; ++i) {
-            if (numberStr.charAt(i) > cmpStr.charAt(i)) {
-                return false;
+            int diff = numberStr.charAt(i) - cmpStr.charAt(i);
+            if (diff != 0) {
+                return (diff < 0);
             }
         }
         return true;
