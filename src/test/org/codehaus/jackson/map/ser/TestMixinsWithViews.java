e9d58f1e2e98d8d77b52076f59f014cf35157c77
Tatu Saloranta <tatu@fasterxml.com>
2011-05-16 23:47:52 +0000

    Add a unit test to verify fix for JACKSON-560

--- src/test/org/codehaus/jackson/map/ser/TestMixinsWithViews.java
+++ src/test/org/codehaus/jackson/map/ser/TestMixinsWithViews.java
@@ -3,6 +3,7 @@ package org.codehaus.jackson.map.ser;
 import java.io.*;
 import java.util.*;
 
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.*;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonView;
@@ -126,6 +127,36 @@ public class TestMixinsWithViews
         static class View { }
     }
     
+    public class A {
+        private String name;
+        private int age;
+        private String surname;
+
+        public A(String name, int age, String surname) { super(); this.name = name; this.age = age; this.surname = surname; }
+
+        public String getName() { return name; }
+
+        public void setName(String name) { this.name = name; }
+
+        public int getAge() { return age; }
+
+        public void setAge(int age) { this.age = age; }
+
+        public String getSurname() { return surname; }
+
+        public void setSurname(String surname) { this.surname = surname; }
+    }
+
+    public interface AView { }
+
+    public abstract class AMixInAnnotation {
+        @JsonProperty("name")
+        @JsonView(AView.class)
+        abstract String getName();
+        @JsonProperty("age") @JsonView(AView.class)
+        abstract int getAge();
+    }
+
     /*
     /**********************************************************
     /* Tests
@@ -134,33 +165,45 @@ public class TestMixinsWithViews
     
     public void testDataBindingUsage( ) throws Exception
     {
-      ObjectMapper objectMapper = createObjectMapper( );
-      String json = serializeWithObjectMapper(new ComplexTestData( ), Views.View.class, objectMapper);
+      ObjectMapper objectMapper = createObjectMapper();
+      ObjectWriter objectWriter = objectMapper.viewWriter(Views.View.class).withDefaultPrettyPrinter();
+      Object object = new ComplexTestData();
+      String json = objectWriter.writeValueAsString(object);
       assertTrue( json.indexOf( "nameHidden" ) == -1 );
       assertTrue( json.indexOf( "\"name\" : \"shown\"" ) > 0 );
     }    
 
+    public void testIssue560() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        A a = new A("myname", 29, "mysurname");
+
+        // Property SerializationConfig.Feature.DEFAULT_VIEW_INCLUSION set to false
+        mapper.configure(SerializationConfig.Feature.DEFAULT_VIEW_INCLUSION, Boolean.FALSE);
+        mapper.getSerializationConfig().addMixInAnnotations(A.class, AMixInAnnotation.class);
+        String json = mapper.viewWriter(AView.class).writeValueAsString(a);
+
+        assertTrue(json.indexOf("\"name\"") > 0);
+    }
+    
+    /*
+    /**********************************************************
+    /* Helper methods
+    /**********************************************************
+     */
+    
     private ObjectMapper createObjectMapper( )
     {
       ObjectMapper objectMapper = new ObjectMapper( );
       objectMapper.configure( SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false );
       objectMapper.getSerializationConfig( ).setSerializationInclusion( JsonSerialize.Inclusion.NON_NULL );
       objectMapper.configure( SerializationConfig.Feature.DEFAULT_VIEW_INCLUSION, false );
-      objectMapper.getSerializationConfig( ).setMixInAnnotations( createMixins( ) );
-      return objectMapper;
-    }
 
-    private Map<Class<?>, Class<?>> createMixins( )
-    {
       Map<Class<?>, Class<?>> sourceMixins = new HashMap<Class<?>, Class<?>>( );
       sourceMixins.put( SimpleTestData.class, TestDataJAXBMixin.class );
       sourceMixins.put( ComplexTestData.class, TestComplexDataJAXBMixin.class );
-      return sourceMixins;
-    }
-
-    private String serializeWithObjectMapper(Object object, Class<?> view, ObjectMapper objectMapper ) throws IOException
-    {
-      ObjectWriter objectWriter = objectMapper.viewWriter(view).withDefaultPrettyPrinter( );
-      return objectWriter.writeValueAsString(object);
+      
+      objectMapper.getSerializationConfig( ).setMixInAnnotations(sourceMixins);
+      return objectMapper;
     }
 }
