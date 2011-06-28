639a162f467c99851dbe95520e8dad7b09052b8d
Tatu Saloranta <tatu@fasterxml.com>
2011-06-28 19:31:47 +0000

    Fix [JACKSON-597]: Improve ClassDeserializer to support primitive types

--- src/mapper/java/org/codehaus/jackson/map/deser/StdDeserializer.java
+++ src/mapper/java/org/codehaus/jackson/map/deser/StdDeserializer.java
@@ -695,6 +695,18 @@ public abstract class StdDeserializer<T>
             JsonToken curr = jp.getCurrentToken();
             // Currently will only accept if given simple class name
             if (curr == JsonToken.VALUE_STRING) {
+                String className = jp.getText();
+                // [JACKSON-597]: support primitive types (and void)
+                if (className.indexOf('.') < 0) {
+                    if ("int".equals(className)) return Integer.TYPE;
+                    if ("long".equals(className)) return Long.TYPE;
+                    if ("float".equals(className)) return Float.TYPE;
+                    if ("double".equals(className)) return Double.TYPE;
+                    if ("byte".equals(className)) return Byte.TYPE;
+                    if ("char".equals(className)) return Character.TYPE;
+                    if ("short".equals(className)) return Short.TYPE;
+                    if ("void".equals(className)) return Void.TYPE;
+                }
                 try {
                     return Class.forName(jp.getText());
                 } catch (ClassNotFoundException e) {
