180285997a86e01246613cbd67a7e4b15c51b566
Tatu Saloranta <tatu@fasterxml.com>
2011-08-26 02:29:55 +0000

    Fixed [JACKSON-668] for 1.8.6

--- src/test/org/codehaus/jackson/map/deser/TestNumbers.java
+++ src/test/org/codehaus/jackson/map/deser/TestNumbers.java
@@ -1,9 +1,12 @@
 package org.codehaus.jackson.map.deser;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import org.codehaus.jackson.*;
 import org.codehaus.jackson.map.*;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
 
 /**
  * Tests related to [JACKSON-139]
@@ -11,6 +14,51 @@ import org.codehaus.jackson.map.*;
 public class TestNumbers
     extends BaseMapTest
 {
+    /*
+    /**********************************************************************
+    /* Helper classes, beans
+    /**********************************************************************
+     */
+
+    static class MyBeanHolder {
+        public Long id;
+        public MyBeanDefaultValue defaultValue;
+    }
+
+    static class MyBeanDefaultValue
+    {
+        public MyBeanValue value;
+    }
+
+    @JsonDeserialize(using=MyBeanDeserializer.class)
+    static class MyBeanValue {
+        public BigDecimal decimal;
+        public MyBeanValue() { this(null); }
+        public MyBeanValue(BigDecimal d) { this.decimal = d; }
+    }
+
+    /*
+    /**********************************************************************
+    /* Helper classes, serializers/deserializers/resolvers
+    /**********************************************************************
+     */
+    
+    static class MyBeanDeserializer extends JsonDeserializer<MyBeanValue>
+    {
+        @Override
+        public MyBeanValue deserialize(JsonParser jp, DeserializationContext ctxt)
+                throws IOException
+        {
+            return new MyBeanValue(jp.getDecimalValue());
+        }
+    }
+
+    /*
+    /**********************************************************************
+    /* Unit tests
+    /**********************************************************************
+     */
+    
     public void testFloatNaN() throws Exception
     {
         ObjectMapper m = new ObjectMapper();
@@ -38,5 +86,36 @@ public class TestNumbers
         assertNull(m.readValue(quote(""), Double.class));
         assertNull(m.readValue(quote(""), BigInteger.class));
         assertNull(m.readValue(quote(""), BigDecimal.class));
+    }
+
+    // // Tests for [JACKSON-668]
+    
+    public void testDeserializeDecimalHappyPath() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\"defaultValue\": { \"value\": 123 } }";
+        MyBeanHolder result = mapper.readValue(json, MyBeanHolder.class);
+        assertEquals(BigDecimal.valueOf(123), result.defaultValue.value.decimal);
+    }
+
+    public void testDeserializeDecimalProperException() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\"defaultValue\": { \"value\": \"123\" } }";
+        try {
+            mapper.readValue(json, MyBeanHolder.class);
+            fail("should have raised exception");
+        } catch (JsonParseException e) {
+            verifyException(e, "not numeric");
+        }
+    }
+
+    public void testDeserializeDecimalProperExceptionWhenIdSet() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\"id\": 5, \"defaultValue\": { \"value\": \"123\" } }";
+        try {
+            MyBeanHolder result = mapper.readValue(json, MyBeanHolder.class);
+            fail("should have raised exception instead value was set to " + result.defaultValue.value.decimal.toString());
+        } catch (JsonParseException e) {
+            verifyException(e, "not numeric");
+        }
     }
 }
